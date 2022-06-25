24376694dc02b3ff44a6a4a98bf33d8024742599
slothmoore <89548403+slothmoore@users.noreply.github.com>
2022-06-25 18:53:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// //Tip Calc
+
+const billTotal = document.querySelector(".bill-totat");
+const tipPercent = document.querySelector(".tip-amount");
+const totalPeople = document.querySelector(".total-people");
+const totalPerPerson = document.querySelector(".each");
+const total = (billTotal * tipPercent) / totalPeople;
+
+const billTotal = document.getElementsByClassName(".bill-total");
