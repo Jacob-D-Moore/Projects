d0551433bf64df8ab18d25f6d15c3e09b3e6dd53
Jacob Moore <89548403+Jacob-D-Moore@users.noreply.github.com>
2022-07-08 21:29:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-document.querySelector(".calculate").addEventListener("click", function () {
+// document.querySelector(".calculate").addEventListener("click", calcTip());
+
+let calcTip = function () {
   let bill = Number(document.querySelector(".bill-total").value);
   let tipPercent = Number(document.querySelector(".tip-amount").value);
   let numPeople = Number(document.querySelector(".total-people").value);
+
+  if (bill === "" && tipPercent === "") {
+    alert("Please enter valid values");
+    return;
+  }
+
   let total = (bill * (tipPercent / 100) + bill) / numPeople;
-
   document.querySelector(".tip").textContent = total;
-});
-
-// (billAmt * serviceQual) / numOfPeople;
+};
+document.querySelector(".calculate").onclick = calcTip;
