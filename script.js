fc0af10fef10dcff405d98796d46ac5abff912f5
Jacob Moore <89548403+Jacob-D-Moore@users.noreply.github.com>
2022-07-29 14:42:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ let calcTip = function () {
   let numPeople = Number(document.querySelector(".total-people").value);
 
   if (bill === "" && tipPercent === "") {
-    alert("Please enter valid values");
+    alert("Please enter valid values.");
     return;
   }
 
+  if (numPeople === "" || numPeople <= 1) {
+    numPeople = 1;
+    document.querySelector(".each").style.display = "none";
+  } else {
+    document.querySelector(".each").style.display = "block";
+  }
+
   let total = (bill * (tipPercent / 100) + bill) / numPeople;
+  total = total.toFixed(2);
   document.querySelector(".tip").textContent = total;
 };
 document.querySelector(".calculate").onclick = calcTip;
